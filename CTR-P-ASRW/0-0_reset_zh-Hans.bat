9c4ea319b09bfeebc07564bfb9b60a400899f432
Sun Daowen <sundaowen@gmail.com>
2019-03-08 17:58:02 +0800

    init CTR-P-ASRW

---
+++ CTR-P-ASRW/0-0_reset_zh-Hans.bat
@@ -0,0 +1,8 @@
+PUSHD "%~dp0"
+RD /S /Q cci
+RD /S /Q build
+DEL "Tohoku Daigaku Karei Igaku Kenkyuusho - Kawashima Ryuuta Kyouju Kanshuu - Monosugoku Nou wo Kitaeru 5-Funkan no Oni Training (TWN) (zh-Hans).3ds"
+DEL patch.3ps
+DEL zh\icon.ico
+DEL zh-Hans_ASRWTOOLS.exe
+POPD
---
+++ CTR-P-ASRW/0-1_reset_zh-Hant.bat
@@ -0,0 +1,8 @@
+PUSHD "%~dp0"
+RD /S /Q cci
+RD /S /Q build
+DEL "Tohoku Daigaku Karei Igaku Kenkyuusho - Kawashima Ryuuta Kyouju Kanshuu - Monosugoku Nou wo Kitaeru 5-Funkan no Oni Training (TWN) (zh-Hant).3ds"
+DEL patch.3ps
+DEL zh\icon.ico
+DEL zh-Hant_ASRWTOOLS.exe
+POPD
