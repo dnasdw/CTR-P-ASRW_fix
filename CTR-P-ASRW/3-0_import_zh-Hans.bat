28e5c318b43bf019b456d1b6ce56c0c70234f13b
Sun Daowen <sundaowen@gmail.com>
2019-03-28 19:05:39 +0800

    update CTR-P-ASRW

--- CTR-P-ASRW/3-0_import_zh-Hans.bat
+++ CTR-P-ASRW/3-0_import_zh-Hans.bat
@@ -2,8 +2,8 @@ PUSHD "%~dp0"
 RD /S /Q build
 MD build\cxi0\exefs
 XCOPY cci\cxi0\exefs build\cxi0\exefs /S /Y
-MD build\cxi0\romfs
-XCOPY cci\cxi0\romfs build\cxi0\romfs /S /Y
+REM MD build\cxi0\romfs
+REM XCOPY cci\cxi0\romfs build\cxi0\romfs /S /Y
 tools\3dstool --lock -vf build\cxi0\exefs\code.bin --region CHN --language CN || PAUSE
 COPY build\cxi0\exefs\banner\banner14\COMMON1.png build\cxi0\exefs\banner\banner0\COMMON1.png
 COPY build\cxi0\exefs\banner\banner14\COMMON1.png build\cxi0\exefs\banner\banner16\COMMON1.png
